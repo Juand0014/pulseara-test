861c813e1d272e64741ad3071affe108656ad0d3
juand0014 <juand0014@hotmail.com>
2022-12-17 20:27:24 -0400

    setting docker and envioraments

--- server/src/config/env.config.ts
+++ server/src/config/env.config.ts
@@ -1,6 +1,7 @@
 export const envConfig = () => ({
 	database: {
-		development: process.env.DB_URI
+		development: process.env.DB_URI,
+		production: process.env.DB_URI_PROD
 	},
 	environment: process.env.NODE_ENV || 'development',
 	dburi: process.env.DB_URI
--- server/src/config/joi.validation.ts
+++ server/src/config/joi.validation.ts
@@ -2,6 +2,7 @@ import * as joi from 'joi';
 
 export const JoiValidationSchema = joi.object({
 	DB_URI: joi.string().required(),
+	DB_URI_PROD: joi.string().required(),
 	NODE_ENV: joi.string().valid('development', 'production', 'test').default('development'),
 	PORT: joi.number().default(3001),
 });
